ad661cbc8af4c4932b52edc74a7ecd74e1c9c753
Tim Zhang <admin@timz.dev>
2024-08-21 21:06:09 +1000

    Change garden to wiki.

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "garden.timz.dev",
+    baseUrl: "wiki.timz.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
